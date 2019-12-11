66c2a2f867960d6df3174572206904f8fb95f1ef
y_megane_IDEA <y.megane88@gmail.com>
2019-12-11 23:31:25 +0900

    update install script

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -1,7 +1,8 @@
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.ssh/config ~/.ssh/config
+DOT="$HOME/develop/dotfiles"
+ln -sf $DOT/.bashrc ~/.bashrc
+ln -sf $DOT/.vimrc ~/.vimrc
+ln -sf $DOT/.ssh/config ~/.ssh/config
 # VSCode
-ln -sf ~/dotfiles/VSCode/User/settings.json ~/Library/ApplicationSupport/Code/User/settings.json 
-ln -sf ~/dotfiles/VSCode/User/snippets/markdown.json ~/Library/ApplicationSupport/Code/User/snippets/markdown.json
-ln -sf ~/dotfiles/VSCode/User/snippets/python.json ~/Library/ApplicationSupport/Code/User/snippets/python.json
+ln -sf $DOT/VSCode/User/settings.json ~/Library/ApplicationSupport/Code/User/settings.json 
+ln -sf $DOT/VSCode/User/snippets/markdown.json ~/Library/ApplicationSupport/Code/User/snippets/markdown.json
+ln -sf $DOT/VSCode/User/snippets/python.json ~/Library/ApplicationSupport/Code/User/snippets/python.json
