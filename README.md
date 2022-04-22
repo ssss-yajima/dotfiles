828dbe93aac119bc4c35a22ab61a3ce1fccfa641
y-megane <y.megane88@gmail.com>
2022-04-22 17:33:35 +0900

    add fish setup scripts

--- README.md
+++ README.md
@@ -2,27 +2,16 @@
 
 ## Setup
 
-### mac
-```sh
-sh mac/install-homebrew.sh
-sh mac/brew-install.sh
-sh mac/karabiner/restore-karabiner.sh
+1. git clone dotfiles
+1. Install fish
+1. Change login shell
+1. Run install script
+1. mv dotfiles repository under ghq dir
 
-sh bin/install-node-from-brew.sh
-sh bin/install-dotfiles.sh
+
+
 ```
-
-## 参考
-- [ようこそdotfilesの世界へ](https://qiita.com/yutakatay/items/c6c7584d9795799ee164)
-- [シンボリックリンクの作成](https://qiita.com/colorrabbit/items/2e99304bd92201261c60)
-- [最強のdotfiles駆動開発](https://qiita.com/b4b4r07/items/b70178e021bef12cd4a2)
-
----
-
-## 変遷
-
-- 2020/11/8
-  - bashからzshに移行
-  - peco導入
-  - snipet運用開始
-  - homebrewの復元を追加
+brew install fish
+chsh -s $(which fish)
+curl -fsSL https://raw.githubusercontent.com/hayashikun/dotfiles/master/setup.fish | fish
+```
---
+++ setup.fish
@@ -0,0 +1,42 @@
+#!/usr/bin/env fish
+
+cd (dirname (status -f)) && source init.fish
+
+for cmd in git curl
+    if not type -q $cmd
+        echo $cmd "is required."; exit 1
+    end
+end
+
+if not test -d $DOT_PATH
+    git clone git@github.com:y-megane/dotfiles.git $DOT_PATH
+end
+
+
+link-file .gitconfig .gitignore_global .zshrc .config/fish/config.fish #.config/fish/fish_plugins
+
+source-config
+
+# make chache dir
+if not test -d $CACHE_PATH
+    mkdir $CACHE_PATH
+end
+
+brew-install jq pwgen tree gh peco ghq
+
+# fisher
+if not test -e $HOME/.config/fish/functions/fisher.fish
+    curl -sL https://git.io/fisher | source
+end
+fisher install (cat $DOT_PATH/.config/fish/fish_plugins)
+
+# powerline
+cd $CACHE_PATH
+if not test -e $CACHE_PATH/font
+    git clone https://github.com/powerline/fonts.git --depth=1
+end
+cd fonts && git pull && ./install.sh
+
+
+
+source-config
